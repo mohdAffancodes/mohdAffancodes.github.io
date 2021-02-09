b6d8bbb863ecc6e60662cfb55f660db4cc8a9a37
mohdAffancodes <78096552+mohdAffancodes@users.noreply.github.com>
2021-02-09 15:08:11 +0530

    added dotype

--- download/dJS.js
+++ download/dJS.js
@@ -1,6 +1,14 @@
 var main = document.querySelector(".main");
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    )
+) {
     main.style.visibility = "hidden";
-    alert("Sorry.Our website is not compatible with your device." + "\r\n" + "And the applications are not made for mobile.")
+    alert(
+        "Sorry.Our website is not compatible with your device." +
+            "\r\n" +
+            "And the applications are not made for mobile."
+    );
 }
